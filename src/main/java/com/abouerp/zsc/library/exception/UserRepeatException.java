b58517682885df938e9f96d07dc7cf3ddcca179f
Abouerp <1057240821@qq.com>
2021-01-05 00:31:42 +0800

    fix exception

--- src/main/java/com/abouerp/zsc/library/exception/UserRepeatException.java
+++ src/main/java/com/abouerp/zsc/library/exception/UserRepeatException.java
@@ -4,7 +4,7 @@ package com.abouerp.zsc.library.exception;
  * @author Abouerp
  */
 public class UserRepeatException extends ClientErrorException {
-    private static final Integer DEFAULT_CODE = 200;
+    private static final Integer DEFAULT_CODE = 4001;
     private static final String DEFAULT_MSG = "User is Existing";
 
     public UserRepeatException() {
