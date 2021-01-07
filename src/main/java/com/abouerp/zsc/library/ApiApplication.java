cc7e49b41e865a8900fe310c5e9edb2139f37823
Abouerp <1057240821@qq.com>
2021-01-07 14:08:51 +0800

    fix BookController

--- src/main/java/com/abouerp/zsc/library/ApiApplication.java
+++ src/main/java/com/abouerp/zsc/library/ApiApplication.java
@@ -2,7 +2,9 @@ package com.abouerp.zsc.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class ApiApplication {
 
