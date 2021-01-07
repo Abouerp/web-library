094847813636aee619c0cc807606146416b06d2a
Abouerp <1057240821@qq.com>
2021-01-07 15:52:07 +0800

    fix log

--- src/main/java/com/abouerp/zsc/library/utils/LoggerUtils.java
+++ src/main/java/com/abouerp/zsc/library/utils/LoggerUtils.java
@@ -1,10 +1,13 @@
 package com.abouerp.zsc.library.utils;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Abouerp
  */
+@Slf4j
 public abstract class LoggerUtils {
     private static final String[] IP_HEADER_CANDIDATES = {
             "X-Forwarded-For",
@@ -23,6 +26,7 @@ public abstract class LoggerUtils {
     public static String getClientIpAddress(HttpServletRequest request) {
         for (String header : IP_HEADER_CANDIDATES) {
             String ip = request.getHeader(header);
+            log.info("ip_header_condidates = {}"+IP_HEADER_CANDIDATES+"    ip = {}"+ip);
             if (ip != null && ip.length() != 0 && !"unknown".equalsIgnoreCase(ip)) {
                 return ip;
             }
