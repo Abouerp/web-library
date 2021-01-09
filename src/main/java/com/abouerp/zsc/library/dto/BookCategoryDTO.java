0da24da4625070d166c05577141cfb53524d2671
Abouerp <1057240821@qq.com>
2021-01-09 17:03:03 +0800

    add return  createBy some information

--- src/main/java/com/abouerp/zsc/library/dto/BookCategoryDTO.java
+++ src/main/java/com/abouerp/zsc/library/dto/BookCategoryDTO.java
@@ -2,6 +2,8 @@ package com.abouerp.zsc.library.dto;
 
 import lombok.Data;
 
+import java.time.Instant;
+
 /**
  * @author Abouerp
  */
@@ -10,4 +12,8 @@ public class BookCategoryDTO {
     private Integer id;
     private String name;
     private String code;
+    private Integer createBy;
+    private Integer updateBy;
+    private Instant createTime;
+    private Instant updateTime;
 }
