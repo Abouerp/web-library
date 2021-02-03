b25f7bbbf61dbcb1c6873e5589ac1978ff3296f4
Abouerp <1057240821@qq.com>
2021-02-03 15:01:36 +0800

    update security preview

--- src/main/java/com/abouerp/zsc/library/security/SecurityConfig.java
+++ src/main/java/com/abouerp/zsc/library/security/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
                 .authorizeRequests()
                 .antMatchers("/api/user/me").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/storage/download/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/storage/download/**","/api/storage/preview/**").permitAll()
                 .antMatchers("/api/search/high").permitAll()
                 .antMatchers("/api/anonymous/**").permitAll()
                 .antMatchers("/api/**").authenticated()
