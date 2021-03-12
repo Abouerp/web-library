0549a9c96dde989d5d6a119a4a35a432146af6fa
Abouerp <1057240821@qq.com>
2021-03-13 00:33:38 +0800

    remove register

--- src/main/java/com/abouerp/zsc/library/controller/AnonymousController.java
+++ src/main/java/com/abouerp/zsc/library/controller/AnonymousController.java
@@ -3,22 +3,15 @@ package com.abouerp.zsc.library.controller;
 import com.abouerp.zsc.library.bean.ResultBean;
 import com.abouerp.zsc.library.domain.book.Book;
 import com.abouerp.zsc.library.domain.book.BookDetail;
-import com.abouerp.zsc.library.domain.user.Administrator;
-import com.abouerp.zsc.library.domain.user.Role;
-import com.abouerp.zsc.library.mapper.AdministratorMapper;
 import com.abouerp.zsc.library.mapper.BookDetailMapper;
 import com.abouerp.zsc.library.repository.BookRepository;
 import com.abouerp.zsc.library.repository.search.BookSearchRepository;
-import com.abouerp.zsc.library.service.AdministratorService;
 import com.abouerp.zsc.library.service.BookDetailService;
 import com.abouerp.zsc.library.service.ProblemManageService;
-import com.abouerp.zsc.library.service.RoleService;
-import com.abouerp.zsc.library.vo.AdministratorVO;
 import com.abouerp.zsc.library.vo.ProblemManageVO;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -37,24 +30,16 @@ public class AnonymousController {
     private final BookRepository bookRepository;
     private final BookDetailService bookDetailService;
     private final BookSearchRepository bookSearchRepository;
-    private final AdministratorService administratorService;
-    private final RoleService roleService;
-    private final PasswordEncoder passwordEncoder;
+
 
     public AnonymousController(ProblemManageService problemManageService,
                                BookRepository bookRepository,
                                BookSearchRepository bookSearchRepository,
-                               BookDetailService bookDetailService,
-                               AdministratorService administratorService,
-                               RoleService roleService,
-                               PasswordEncoder passwordEncoder) {
+                               BookDetailService bookDetailService) {
         this.problemManageService = problemManageService;
         this.bookRepository = bookRepository;
         this.bookSearchRepository = bookSearchRepository;
         this.bookDetailService = bookDetailService;
-        this.administratorService = administratorService;
-        this.roleService = roleService;
-        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/sync")
@@ -93,33 +78,5 @@ public class AnonymousController {
             map.put("book", bookRepository.findById(id));
         }
         return ResultBean.ok(map);
-    }
-
-    @PostMapping("/user/register")
-    public ResultBean register(@RequestBody AdministratorVO administratorVO) {
-        Role role = roleService.findFirstByIsDefault(true);
-        if (role == null) {
-            return ResultBean.of(200, "Can't find default role");
-        }
-        Administrator administrator = AdministratorMapper.INSTANCE.toAdmin(administratorVO);
-        administrator.setPassword(passwordEncoder.encode(administratorVO.getPassword()));
-        //todo have bug, remove createBy AND updateBy OR remove register
-        administrator.setCreateBy("anonymous");
-        administrator.setUpdateBy(administratorVO.getUsername());
-        Set<Role> roles = new HashSet<>();
-        roles.add(role);
-        administrator.setRoles(roles);
-        return ResultBean.ok(AdministratorMapper.INSTANCE.toDTO(administratorService.save(administrator)));
-    }
-
-    /**
-     *
-     * @param username
-     * @return true ：存在
-     *          false：不存在
-     */
-    @GetMapping("/user/exist")
-    public ResultBean existsByUserName(@RequestParam String username){
-        return ResultBean.ok(administratorService.existsByUserName(username));
     }
 }
